86c354129f691ad52d3f7a53abcddeb29ab3470d
joao <joao182cx@gmail.com>
2019-01-09 12:55:44 -0200

    Criação do getters e setter da classe.

--- src/Conta.java
+++ src/Conta.java
@@ -27,6 +27,23 @@ public class Conta {
 		}
 		
 	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(BigDecimal saldo) {
+		this.saldo = saldo;
+	}
+	
 	
 	
 	
